60098044daba393d221126e848e0536a8abe7ba1
idev <khanhcongpruj123@gmail.com>
2024-05-28 23:31:43 +0700

    update: paginate

--- domain/common/dto/paging.go
+++ domain/common/dto/paging.go
@@ -1,7 +1,13 @@
 package commondto
 
-type Paging struct {
-	Data  []any  `json:"data,omitempty"`
-	Total uint64 `json:"total,omitempty"`
-	Page  uint64 `json:"page,omitempty"`
+type Pagable struct {
+	Total int64 `json:"total,omitempty" query:"total"`
+	Page  int64 `json:"page,omitempty" query:"page"`
+	Size  int64 `json:"size,omitempty" query:"size"`
+	All   int64 `json:"all,omitempty" query:"all"`
+}
+
+type Page struct {
+	Pagable
+	Data []any `json:"data,omitempty" query:"data"`
 }
