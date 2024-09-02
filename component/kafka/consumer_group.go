e7fb1eba35e6527c59ba341bdbb03623ce9fe2b8
idev3105 <khanhcongpruj123@gmail.com>
2024-09-03 01:27:00 +0700

    update: refactor consumer group

--- component/kafka/consumer_group.go
+++ component/kafka/consumer_group.go
@@ -21,7 +21,7 @@ type ConsumerGroup struct {
 }
 
 func NewConsumerGroup(host string, port int32, groupID string, topics []string, onReceiveFunc func(context.Context, []byte) error) (*ConsumerGroup, error) {
-	addrs := []string{fmt.Sprintf("%v:%v", host, port)}
+	addrs := []string{fmt.Sprintf("%s:%d", host, port)}
 
 	cfg := sarama.NewConfig()
 	cfg.Consumer.Group.Rebalance.GroupStrategies = []sarama.BalanceStrategy{sarama.NewBalanceStrategyRoundRobin()}
@@ -29,8 +29,9 @@ func NewConsumerGroup(host string, port int32, groupID string, topics []string, 
 
 	cg, err := sarama.NewConsumerGroup(addrs, groupID, cfg)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to create consumer group: %w", err)
 	}
+
 	return &ConsumerGroup{
 		Host:          host,
 		Port:          port,
@@ -39,25 +40,21 @@ func NewConsumerGroup(host string, port int32, groupID string, topics []string, 
 		OnReceiveFunc: onReceiveFunc,
 		Ready:         make(chan bool),
 		consumerGroup: cg,
-	}, err
+	}, nil
 }
 
 func (k *ConsumerGroup) Start(ctx context.Context) error {
 	log := logger.New("ConsumerGroup", "Start")
 
-	log.Info("Start this consumer in group: ", k.GroupID)
+	log.Info("Starting consumer in group: ", k.GroupID)
 	for {
-		// `Consume` should be called inside an infinite loop, when a
-		// server-side rebalance happens, the consumer session will need to be
-		// recreated to get the new claims
 		if err := k.consumerGroup.Consume(ctx, k.Topics, k); err != nil {
 			if errors.Is(err, sarama.ErrClosedConsumerGroup) {
 				return nil
 			}
-			log.Errorf("Error from consumer: %v", err)
-			return err
+			return fmt.Errorf("error from consumer: %w", err)
 		}
-		// check if context was cancelled, signaling that the consumer should stop
+
 		if ctx.Err() != nil {
 			return nil
 		}
@@ -67,47 +64,38 @@ func (k *ConsumerGroup) Start(ctx context.Context) error {
 
 func (k *ConsumerGroup) Close() error {
 	log := logger.New("ConsumerGroup", "Close")
-	log.Info("Close this consumer")
+	log.Info("Closing consumer")
 	if err := k.consumerGroup.Close(); err != nil {
-		log.Errorf("Error closing consumer group: %v", err)
-		return err
+		return fmt.Errorf("error closing consumer group: %w", err)
 	}
 	return nil
 }
 
-// Implement sarama.ConsumerGroupHandler interface
-func (k *ConsumerGroup) Setup(session sarama.ConsumerGroupSession) error {
+func (k *ConsumerGroup) Setup(sarama.ConsumerGroupSession) error {
 	close(k.Ready)
 	return nil
 }
 
-// Implement sarama.ConsumerGroupHandler interface
-func (k *ConsumerGroup) Cleanup(session sarama.ConsumerGroupSession) error {
-	// TODO: Implement here
+func (k *ConsumerGroup) Cleanup(sarama.ConsumerGroupSession) error {
 	return nil
 }
 
-// Implement sarama.ConsumerGroupHandler interface
 func (k *ConsumerGroup) ConsumeClaim(session sarama.ConsumerGroupSession, claim sarama.ConsumerGroupClaim) error {
-	logger := logger.New("ConsumerGroup", "ConsumeClaim")
+	log := logger.New("ConsumerGroup", "ConsumeClaim")
 	for {
 		select {
 		case message, ok := <-claim.Messages():
 			if !ok {
-				logger.Info("Message channel was closed")
+				log.Info("Message channel was closed")
 				return nil
 			}
-			logger.Infof("Message claimed: value = %s, timestamp = %v, topic = %s", string(message.Value), message.Timestamp, message.Topic)
+			log.Infof("Message claimed: value = %s, timestamp = %v, topic = %s", string(message.Value), message.Timestamp, message.Topic)
 
-			err := k.OnReceiveFunc(session.Context(), message.Value)
-			if err != nil {
-				return err
+			if err := k.OnReceiveFunc(session.Context(), message.Value); err != nil {
+				return fmt.Errorf("error processing message: %w", err)
 			}
 
 			session.MarkMessage(message, "")
-		// Should return when `session.Context()` is done.
-		// If not, will raise `ErrRebalanceInProgress` or `read tcp <ip>:<port>: i/o timeout` when kafka rebalance. see:
-		// https://github.com/IBM/sarama/issues/1192
 		case <-session.Context().Done():
 			return nil
 		}
